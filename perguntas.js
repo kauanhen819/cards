40e7d9db09721810c7797a329a3906f6fcd06c72
kauanhen819 <160596646+kauanhen819@users.noreply.github.com>
2024-11-05 08:02:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,69 @@
+criaCartao(
+    'voleibol',
+    'quantos jogadores tem que sempre estar em quadra?',
+    'sempre tem que ter 6 jogadores em quadra'
+)
+
+criaCartao(
+    'voleibol',
+    'como funciona o rodizio?',
+    '2 jogadores serão sempre diagonais um do outro dentro de quadra '
+)
+
+criaCartao(
+    'voleibol',
+    'como funciona a minutagem do jogo?',
+    'e divido por sets de 25 pontos'
+)
+
+criaCartao(
+    'voleibol',
+    'quais as posições dos jogadores?',
+    'ponteiro, levantador, central e oposto'
+)
+
+criaCartao(
+    'voleibol',
+    'quantas vezes um time pode encostar na bola?',
+    'pode ter 3 toques da mesma equipe em uma jogada'
+)
+criaCartao(
+    'voleibol',
+    'pode acontecer empate?',
+    'não, o empate e reolvido em mais um set de 15 pontos'
+)
+criaCartao(
+    'voleibol',
+    'quais as linhas do voleibol?',
+    'linha central, linha de fundo e linha lateral'
+)
+
+criaCartao(
+    'voleibol',
+    'o que e set point?',
+    'e o set que pode definir o vencedor'
+)
+
+criaCartao(
+    'voleibol',
+    'qual a altura da rede masculina?',
+    'no masculino são 2.43 metros'
+) 
+
+criaCartao(
+    'voleibol',
+    'qual a altura da rede no feminino?',
+    'no feminino são 2,24 metros'
+)
+
+criaCartao(
+    'voleibol',
+    'quais os sistemas de jogo?',
+    '6x0, 5x1, 3x3 e 4x2'
+) 
+
+criaCartao(
+    'voleibol',
+    'quais são as areas de jogo?',
+    'zona livre e quadra de jogo'
+)
